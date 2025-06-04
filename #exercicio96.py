b92cc0c8e20aba49bbd39d224e60c67afa3b6fe2
Thiago5731 <thiagov88@outlook.com>
2025-06-04 13:46:39 -0300

    Add files via upload

---
+++ #exercicio96.py
@@ -0,0 +1,14 @@
+#exercicio96
+
+#função area
+def area(a, b):
+    resultado = a * b
+    print (f'A area desse terreno é: {resultado} ')
+
+
+#programa principal
+a = float(input('Digite a LARGURA (m): '))
+b = float(input('Digite o COMPRIMENTO (m): '))
+
+#resultado
+area(a, b)

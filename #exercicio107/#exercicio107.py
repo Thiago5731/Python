2d4817c34b7a19520be769b99c9e1d08b4bd8f83
Thiago5731 <thiagov88@outlook.com>
2025-08-06 12:13:38 -0300

    Add files via upload

---
+++ #exercicio107/#exercicio107.py
@@ -0,0 +1,7 @@
+#exercicio107
+import metade_dobro_porcentagem
+#programa principal
+numero = int(input('Digite um numero: '))
+print(f'A metade de {numero} é: {metade_dobro_porcentagem.metade(numero)}')
+print(f'O dobro de {numero} é: {metade_dobro_porcentagem.dobro(numero)}')
+print(f'10% + de {numero} é: {metade_dobro_porcentagem.porcentagem(numero)}')
---
+++ #exercicio107/#metade_dobro_porcentagem.py
@@ -0,0 +1,19 @@
+#metade_dobro_porcentagem
+
+#metade
+def metade(numero):
+    resultado = numero/2
+    return resultado
+
+
+#dobro
+def dobro(numero):
+    resultado = numero*2
+    return resultado
+
+
+#porcentagem_aumentado em 10%
+def porcentagem(numero):
+    dez = numero/10
+    resultado = dez + numero
+    return resultado
